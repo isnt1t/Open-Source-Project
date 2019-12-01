7a6db44ca3cc3c5f28889444b8d60b9d7030115f
isnt1t <standingchild@gmail.com>
2019-12-02 00:09:28 +0900

    DB_Listview

---
+++ DB_Listview/app/src/main/java/com/example/sqlistefromasset/database/DatabaseHelper.java
@@ -0,0 +1,66 @@
+package com.example.sqlistefromasset.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.sqlistefromasset.model.Product;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DBNAME = "CVS_DATA.db";
+    public static final String DBLOCATION = "/data/data/com.example.sqlistefromasset/databases/";
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public DatabaseHelper(Context context){
+        super(context, DBNAME, null , 1);
+        this.mContext = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void openDatabase(){
+        String dbPath = mContext.getDatabasePath(DBNAME).getPath();
+        if(mDatabase != null && mDatabase.isOpen()){
+            return;
+        }
+        mDatabase = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public void closeDatabase(){
+        if(mDatabase != null){
+            mDatabase.close();
+        }
+    }
+
+    public List<Product> getListProduct() {
+        Product product = null;
+        List<Product> productList = new ArrayList<>();
+        openDatabase();
+        Cursor cursor = mDatabase.rawQuery("SELECT id, product_name, price, cvs_name FROM CVS_DATA", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            product = new Product(cursor.getInt(0),
+                                cursor.getString(1),
+                                cursor.getInt(2),
+                                cursor.getString(3));
+            productList.add(product);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        closeDatabase();
+        return productList;
+    }
+}
